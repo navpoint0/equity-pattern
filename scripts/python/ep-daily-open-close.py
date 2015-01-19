ebffd66ebd5353aed652f56e21abd3bde211794b
navpoint0 <navpoint0@gmail.com>
2015-01-18 22:25:55 -0800

    Create ep-daily-open-close.py

---
+++ scripts/python/ep-daily-open-close.py
@@ -0,0 +1,28 @@
+import requests
+import psycopg2
+import sys
+
+url = "http://tiny.cc/ep-stock/?f=ol1&s="
+
+con = None
+try:
+    con = psycopg2.connect(database='equities', user='postgres', host='localhost', password='b10bubb@')
+    cur = con.cursor()
+    cur.execute('SELECT id, ticker, CURRENT_DATE - tracking_date FROM equities.stock_master WHERE (CURRENT_DATE - tracking_date) < 5')
+    rows = cur.fetchall()
+    for row in rows:
+        results = requests.get(url + row[1]).content
+        oc = results.strip('\r\n').split(',')
+        opn = 0 if oc[0] == 'N/A' else oc[0]
+        cur.execute("INSERT INTO equities.stock_data (id, stock_id, date, trend_days, open_price, close_price) VALUES (DEFAULT, %s, CURRENT_DATE, %s, %s, %s)", (row[0], row[2], opn, oc[1]))
+    con.commit()
+except psycopg2.DatabaseError, e:   
+    if con:
+        con.rollback()    
+    print 'Error %s' % e    
+    sys.exit(1)        
+finally:   
+    if con:
+        con.close()
+
+
